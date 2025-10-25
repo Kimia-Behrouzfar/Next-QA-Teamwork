951b8322f21e3e0ff38a2ecd5eb1b8652605731e
kimiabeh <110967737+kimiabeh@users.noreply.github.com>
2025-10-25 23:05:12 +0330

    setting up package and next.js 15

--- app/page.jsx
+++ app/page.jsx
@@ -1,11 +1,3 @@
-import Image from "next/image";
-import styles from "./page.module.css";
-import Form from "./components/Form";
-
 export default function Home() {
-  return (
-    <>
-      <Form />
-    </>
-  );
+  return <></>;
 }
